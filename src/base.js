98244703489dc56f154bffe218695578ecdfa016
oyenuga17 <cicinuga@gmail.com>
2021-05-11 11:03:51 +0100

    intial commit

---
+++ src/base.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+export const app = firebase.initializeApp({
+  apiKey: "AIzaSyAm-aI6edKdfEhy16NOtYEVHEwgrjGKx10",
+    authDomain: "hosting-ec33f.firebaseapp.com",
+    projectId: "hosting-ec33f",
+    storageBucket: "hosting-ec33f.appspot.com",
+    messagingSenderId: "544209830816",
+    appId: "1:544209830816:web:2297b09173094b99361ac5"
+});
